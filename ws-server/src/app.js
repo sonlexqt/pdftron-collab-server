e2b5fcc48fea4a96c3020e24a88eabcc8ce6c3f4
hoanghaiha <hahh@dgroup.co>
2020-05-13 14:52:06 +0700

    Create chat rooms

--- ws-server/src/app.js
+++ ws-server/src/app.js
@@ -17,17 +17,43 @@ server.on('listening', () => {
 })
 
 const io = SocketIO(server)
-io.on('connection', (socket) => {
-  console.log(`${socket.id} connected`)
-  socket.on('disconnect', () => {
-    console.log(`user ${socket.id} disconnected`)
-  })
+// io.on('connection', (socket) => {
+//   console.log(`${socket.id} connected`)
+//   socket.on('disconnect', () => {
+//     console.log(`user ${socket.id} disconnected`)
+//   })
+//   socket.on('annotationChanged', (data) => {
+//     console.log(`${socket.id} annotationChanged`)
+//     // send request to api-server
+//     axios.post('http://api-server:3000/api/annotations', data)
+//     socket.broadcast.emit('annotationUpdated', data)
+//   })
+// })
+
+io.sockets.on('connection',  (socket) => {
+	
+	socket.on('userJoinRoom', (user)=>{
+		socket.username = user.userName;
+		socket.room = user.room;
+		socket.join(socket.room);
+		// socket.emit('updateInfo', 'SERVER', 'you have connected to ' + socket.room);
+		// socket.broadcast.to(socket.room).emit('updateInfo', 'SERVER', socket.username + ' has connected to this room');
+		console.log(  'SERVER', socket.username + ' has connected to room ' + socket.room );
+  });
+  
   socket.on('annotationChanged', (data) => {
-    console.log(`${socket.id} annotationChanged`)
-    // send request to api-server
-    axios.post('http://api-server:3000/api/annotations', data)
-    socket.broadcast.emit('annotationUpdated', data)
-  })
-})
+        console.log(`${socket.id} annotationChanged`)
+        // send request to api-server
+        axios.post('http://api-server:3000/api/annotations', data)
+        io.sockets.in(socket.room).emit('annotationUpdated', data);
+      })
+	
+
+	socket.on('disconnect', ()=>{
+		console.log( socket.username + ' has disconnected');
+		socket.leave(socket.room);
+	});
+});
+
 
 app.get('/', (req, res) => res.send('ws-server'))
